21ea00f3a4dc05236f973fc9b950d3045d8b7ffd
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2023-12-31 11:22:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const updateFoodPosition = () => {
 const handleGameOver = () => {
     // Clearing the timer and reloading the page on game over
     clearInterval(setIntervalId);
-    alert( MR KIKSY: "Game Over! You are a failure, you can never make it  Press OK if you  AGREE...");
+    alert( " MR KIKSY: Game Over! You are a failure, you can never make it  Press OK if you  AGREE...");
     location.reload();
 }
 
